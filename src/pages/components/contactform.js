37afd3c65d4376266fa22bb4942d2c665989fc7d
Sozai83 <chikushi3.227@gmail.com>
2023-05-10 23:11:59 +1000

    added functioning contact form

--- src/pages/components/contactform.js
+++ src/pages/components/contactform.js
@@ -1,27 +1,63 @@
 import Image from 'next/image'
 import Link from 'next/link'
+import {useState} from 'react';
 import withWaveSection from '../wavelayout'
-import Styles from '../../styles/home/Contact.module.scss'
+import Styles from '../../styles/home/ContactForm.module.scss'
 
 //Home page - Skills/Tech stack section
 export default function Contact() {
+    const [fname, setFname] = useState('');
+    const [lname, setLname] = useState('');
+    const [email, setEmail] = useState('');
+    const [query, setQuery] = useState('');
+
 
   return (
-          <form action="https://formsubmit.co/chikushi3.227@gmail.com" method="POST">
+          <form className={Styles.form} action="https://formsubmit.co/chikushi3.227@gmail.com" method="POST">
             {/* honepot */}
             <input type="text" name="_honey" style={{'display': 'none'}}/>
             {/* disable captcha */}
             <input type="hidden" name="_captcha" value="false"/>
-            <input type="hidden" name="_honey" style={{'display': 'none'}}/>
-            <label for="fname">First Name: </label>
-            <input type="text" name="fname" required/>
-            <label for="lname">Last Name: </label>
-            <input type="text" name="lname" required/>
-            <label for="email">Email: </label>
-            <input type="email" name="email" required/>
-            <label for="email">Email: </label>
-            <input type="email" name="email" required/>
-            <button type="submit">Send a email to me!</button>
+            <div className={Styles.name}>
+                <label for="fname">First Name: </label>
+                <input 
+                    type="text" 
+                    name="fname"
+                    id="fname"
+                    value={fname} 
+                    onChange={(e)=>{setFname(e.target.value)}} 
+                    required/>
+
+                <label for="lname">Last Name: </label>
+                <input                    
+                    type="text" 
+                    name="lname" 
+                    id="lname"
+                    value={lname} 
+                    onChange={(e)=>{setLname(e.target.value)}} 
+                    required/>
+            </div>
+            <div className={Styles.email}>
+                <label for="email">Email: </label>
+                <input
+                    type="email" 
+                    name="email" 
+                    id="email"
+                    value={email} 
+                    onChange={(e)=>{setEmail(e.target.value)}} 
+                    required/>
+            </div>
+            <div className={Styles.query}>
+                <label for="query">Questions: </label>
+                <textarea
+                    type="query" 
+                    name="query" 
+                    id="query"
+                    value={query} 
+                    onChange={(e)=>{setQuery(e.target.value)}} 
+                    required/>
+            </div>
+            <button type="submit">Send an email to me!</button>
           </form>
 
   )
