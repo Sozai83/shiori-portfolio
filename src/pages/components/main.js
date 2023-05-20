8eb862fc69af40cfd97034615fc8add908486f8c
Sozai83 <chikushi3.227@gmail.com>
2023-05-20 16:43:43 +1000

    added useRef for the main page

--- src/pages/components/main.js
+++ src/pages/components/main.js
@@ -7,19 +7,29 @@ import ShortAboutMe from './short_about_me'
 import Contact from './contact'
 import Footer from '../footer'
 import { Inter } from 'next/font/google'
+import {useRef} from 'react'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Main() {
+  const welcome = useRef();
+  const recentWork = useRef();
+  const aboutMe = useRef();
+  const contact = useRef();
+
   return (
     <>
     <Nav tempActive="topnav-home"/>
     <main>
         <SideNav/>
-        <Welcome name=""/>
-        <RecentWork/>
-        <ShortAboutMe/>
-        <Contact/>
+        <Welcome 
+          ref={welcome}/>
+        <RecentWork 
+          ref={recentWork}/>
+        <ShortAboutMe 
+          ref={aboutMe}/>
+        <Contact 
+          ref={contact}/>
         <Footer/>
     </main>
     </>
