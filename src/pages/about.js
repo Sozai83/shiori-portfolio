2fb5cd6bf55a98a25dc1dfdc046508803ac110af
Sozai83 <chikushi3.227@gmail.com>
2023-06-13 18:44:00 +0900

    Added withWave function in the main page

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,15 +1,17 @@
 import Image from 'next/image'
 import Link from 'next/link'
 import Nav from './components/nav'
-import AboutContent from './components/about/about'
+import AboutContent from './components/about/aboutWrapper'
 import Footer from './footer'
 import withWaveSection from './wavelayout'
+
+const AboutContentWave = withWaveSection(AboutContent, '#EEEDE7', 'white');
 
 export default function About() {
   return (
     <>
         <Nav bgcolor="white"/>
-        <AboutContent/>
+        <AboutContentWave/>
         <Footer/>
     </>
   )
--- src/pages/components/about/aboutWrapper.js
+++ src/pages/components/about/aboutWrapper.js
@@ -2,10 +2,9 @@ import Image from 'next/image'
 import Link from 'next/link'
 import Introduction from './introduction'
 import TechStack from './techstack'
-import withWaveSection from '../../wavelayout'
 import Styles from '../../../styles/about/about.module.scss'
 
-function About() {
+export default function About() {
   return (
     <section className={Styles.wrapper}>
       <Introduction/>
@@ -13,5 +12,3 @@ function About() {
     </section>
   )
 }
-
-export default withWaveSection(About, '#EEEDE7', 'white');
